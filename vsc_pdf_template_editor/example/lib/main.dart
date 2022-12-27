ef1746f51c1ace56aa789dad23045adb691d48e2
dsyrstad <dsyrstad@vscorp.com>
2022-12-27 12:42:52 -0600

    Update expressions version. PR was merged

--- vsc_pdf_template_editor/example/lib/main.dart
+++ vsc_pdf_template_editor/example/lib/main.dart
@@ -1,8 +1,8 @@
 import 'dart:convert';
 
 import 'package:flutter/material.dart';
-import 'package:vsc_pdf_template_editor/vsc_pdf_template_editor.dart';
 import 'package:flutter/services.dart' show rootBundle;
+import 'package:vsc_pdf_template_editor/vsc_pdf_template_editor.dart';
 
 void main() {
   runApp(const EditorApp());
@@ -44,13 +44,19 @@ class _EditorAppState extends State<EditorApp> {
   }
 
   Future<List<Map<String, dynamic>>> _loadData() async {
-    final sampleData = await _getSampleTemplate();
-    final sampleContext = await _getData();
-    return [sampleData, sampleContext];
+    try {
+      final sampleData = await _getSampleTemplate();
+      final sampleContext = await _getData();
+      return [sampleData, sampleContext];
+    } catch (e, st) {
+      print('Failed to load data: $e\n$st');
+      rethrow;
+    }
   }
 
   Future<Map<String, dynamic>> _getSampleTemplate() async {
-    final res = await rootBundle.loadString('assets/test/sample_text.json');
+    final res =
+        await rootBundle.loadString('assets/test/invoice_tpl_test.json');
     final result = Map<String, dynamic>.from(json.decode(res));
     return result;
   }
