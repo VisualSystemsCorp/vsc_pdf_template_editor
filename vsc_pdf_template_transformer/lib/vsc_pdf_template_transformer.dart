8c7edddd01869618e19d4be6e225e09a09938d23
dsyrstad <dsyrstad@vscorp.com>
2025-07-08 09:52:33 -0500

    Export google font handling

--- vsc_pdf_template_transformer/lib/vsc_pdf_template_transformer.dart
+++ vsc_pdf_template_transformer/lib/vsc_pdf_template_transformer.dart
@@ -1,5 +1,5 @@
-library vsc_pdf_template_transformer;
-
+export 'src/fonts/font.dart';
+export 'src/fonts/gfonts.dart';
 export 'src/network/cache.dart';
 export 'src/vsc_pdf_template_transformer_base.dart' show Transformer;
 export 'utils/evaluator.dart';
